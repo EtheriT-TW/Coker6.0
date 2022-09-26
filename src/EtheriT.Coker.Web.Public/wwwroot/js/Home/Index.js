e3777b712a6ced20fcc13b0eec2d1fe6c5ed3ece
PeelSummer <m1020117@gmail.com>
2022-09-26 17:33:50 +0800

    9/26工作內容

---
+++ src/EtheriT.Coker.Web.Public/wwwroot/js/Home/Index.js
@@ -0,0 +1,10 @@
+﻿
+function PageReady() {
+    $(document).ready(function () {
+        $("#btn_gonews").on("click", gonews);
+    })
+}
+
+function gonews() {
+    $('html, body').animate({ scrollTop: $("#News").offset().top }, 0);
+}

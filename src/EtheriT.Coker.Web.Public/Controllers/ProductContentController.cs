32c5b9a3801027a17a0d5f222f655fa772d8eb25
PeelSummer <m1020117@gmail.com>
2022-09-28 18:29:56 +0800

    9/28工作進度

---
+++ src/EtheriT.Coker.Web.Public/Controllers/ProductContentController.cs
@@ -0,0 +1,27 @@
+﻿using EtheriT.Coker.Web.Public.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EtheriT.Coker.Web.Public.Controllers
+{
+	public class ProductContentController : Controller
+	{
+        private readonly ILogger<ProductContentController> _logger;
+
+        public ProductContentController(ILogger<ProductContentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

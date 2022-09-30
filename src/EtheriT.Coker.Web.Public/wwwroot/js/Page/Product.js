6f43b75a37ef64f9fe7921ac5476e94498d3eccf
PeelSummer <m1020117@gmail.com>
2022-09-30 11:49:35 +0800

    商品目錄頁面、商品內容頁面、其他內容微調 商品目錄頁面： - 分類改用 container class - 最新型錄區塊自適應 - 頂部頁面路徑 - 商品加上連結 - Swiper改名 商品內容頁面： - 頂部頁面路徑 - 影片大小微調 其他： -資料夾重新命名 -路徑名稱修改 -新增Footer Init()

--- src/EtheriT.Coker.Web.Public/wwwroot/js/Page/Product.js
+++ src/EtheriT.Coker.Web.Public/wwwroot/js/Page/Product.js
@@ -1,5 +1,5 @@
 ﻿function PageReady() {
-    var product_swiper = new Swiper(".GuessYouLikeSwiper", {
+    var guess_you_like_swiper = new Swiper(".GuessYouLikeSwiper", {
         slidesPerView: jQuery(window).width() > 768 ? 4 : jQuery(window).width() > 576 ? 3 : jQuery(window).width() >= 375 ? 2 : 1,
         spaceBetween: 15,
         loop: true,
@@ -9,7 +9,7 @@
         },
     });
 
-    var product_swiper = new Swiper(".HotProductsSwiper", {
+    var hot_products_swiper = new Swiper(".HotProductsSwiper", {
         slidesPerView: jQuery(window).width() > 768 ? 4 : jQuery(window).width() > 576 ? 3 : jQuery(window).width() >= 375 ? 2 : 1,
         spaceBetween: 15,
         loop: true,
@@ -19,7 +19,7 @@
         },
     });
 
-    var product_swiper = new Swiper(".RelatedProductsSwiper", {
+    var related_products_swiper = new Swiper(".RelatedProductsSwiper", {
         slidesPerView: jQuery(window).width() > 768 ? 4 : jQuery(window).width() > 576 ? 3 : jQuery(window).width() >= 375 ? 2 : 1,
         spaceBetween: 15,
         loop: true,

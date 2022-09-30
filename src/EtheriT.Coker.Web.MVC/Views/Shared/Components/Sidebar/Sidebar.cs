873f37b04605c3b5c783fcd1ddc789547ce85840
charles1984s <charles@ether.com.tw>
2022-09-30 18:16:13 +0800

    [update] 增加後臺頁面框架，版型顏色選擇

---
+++ src/EtheriT.Coker.Web.MVC/Views/Shared/Components/Sidebar/Sidebar.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EtheriT.Coker.Web.MVC.Views.Shared.Components.Sidebar
+{
+    public class Sidebar : ViewComponent
+    {
+        public Sidebar() { 
+        
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}

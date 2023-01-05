4b0850097b3d912cc4ecf041512de6975e22e07c
PeelSummer <m1020117@gmail.com>
2023-01-05 10:04:50 +0800

    [update] 建立共用css與js

---
+++ src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/ViewTypeChange.js
@@ -0,0 +1,49 @@
+﻿function ViewTypeChangeInit() {
+    $(".frame").each(function () {
+        var $self = $(this)
+        if ($self.data("action") == "TypeChange") {
+            var $btn_grid = $self.find(".btn_grid");
+            var $btn_list = $self.find(".btn_list");
+            var $content = $self.find(".row").first();
+
+            $btn_grid.on("click", function () {
+                if (!$btn_grid.data("activate")) {
+                    typeChange($btn_grid, $btn_list, $content, "Grid");
+                }
+            })
+
+            $btn_list.on("click", function () {
+                if (!$btn_list.data("activate")) {
+                    typeChange($btn_list, $btn_grid, $content, "List");
+                }
+            })
+        }
+    })
+}
+
+function typeChange($self, $brother, $content, type) {
+    $self.data("activate", 1);
+    $self.removeClass("text-black-50");
+    $brother.data("activate", 0);
+    $brother.addClass("text-black-50");
+    if (type == "List") {
+        $content.each(function () {
+            var $self = $(this)
+            $self.removeClass("row row-cols-2 row-cols-sm-4");
+            $self.find("figure").removeClass("flex-column");
+            $self.find("figcaption").addClass("flex-grow-1 p-3");
+            $self.find(".title").removeClass("text-center");
+            $self.find(".text").removeClass("d-none");
+        })
+    } else if (type == "Grid") {
+        $content.each(function () {
+            var $self = $(this)
+            $self.addClass("row row-cols-2 row-cols-sm-4");
+            $self.find("figure").addClass("flex-column");
+            $self.find("figcaption").removeClass("flex-grow-1 p-3");
+            $self.find(".title").addClass("text-center");
+            $self.find(".text").addClass("d-none");
+
+        })
+    }
+}

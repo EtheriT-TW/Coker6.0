2f8affe11b024e0a2b1bca685d6115536bf71d89
PeelSummer <m1020117@gmail.com>
2023-06-06 16:32:20 +0800

    [update] 補前次漏傳

--- src/EtheriT.Coker.Application.Shared/WebMenu/IWebMenuApplication.cs
+++ src/EtheriT.Coker.Application.Shared/WebMenu/IWebMenuApplication.cs
@@ -18,6 +18,7 @@ namespace EtheriT.Coker.Application
         public Task<ResponseMessageDto> CreateOrEdit(MenuItemDto dto);
         public Task<GetMenuContenDto> GetConten(SearchIDDto dto);
         public Task<GetFrontContenOutputDto> GetFrontConten(GetFrontContenInputDto dto);
+        public Task<List<GetMenuBreadDto>> GetMenuBread(long Id);
         public Task<ResponseMessageDto> saveConten(MenuSaveContenDto dto);
         public Task<ResponseMessageDto> importConten(MenuSaveContenDto dto);
         public Task<ResponseMessageDto> Delete(DataDelectDto dto);

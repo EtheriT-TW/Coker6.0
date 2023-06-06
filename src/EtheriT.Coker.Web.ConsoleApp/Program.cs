ed979b596994a0028c3902cf82819a17d5e3dbdd
charles1984s <charles@ether.com.tw>
2023-06-06 16:49:10 +0800

    [update] 增加資料舊轉新專案

---
+++ src/EtheriT.Coker.Web.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+// load the configuration file.
+var configBuilder = new ConfigurationBuilder().
+   AddJsonFile("appsettings.json").Build();
+
+// get the section to read
+var configSection = configBuilder.GetSection("ConnectionStrings");
+
+// get the configuration values in the section.
+var source = configSection["source"] ?? null;
+Console.WriteLine(source);

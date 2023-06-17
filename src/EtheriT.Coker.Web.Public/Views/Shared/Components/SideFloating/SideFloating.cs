302434a6ce7dfd31a14268fe787fe4f3389b3e50
PeelSummer <m1020117@gmail.com>
2023-06-17 09:19:29 +0800

    [update] 右側廣告前台顯示調整

--- src/EtheriT.Coker.Web.Public/Views/Shared/Components/SideFloating/SideFloating.cs
+++ src/EtheriT.Coker.Web.Public/Views/Shared/Components/SideFloating/SideFloating.cs
@@ -1,4 +1,5 @@
-﻿using EtheriT.Coker.Application.Shared.Dto.HtmlContent;
+﻿using EtheriT.Coker.Application;
+using EtheriT.Coker.Application.Shared.Dto.HtmlContent;
 using EtheriT.Coker.Application.Shared.HtmlContent;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -10,18 +11,28 @@ namespace EtheriT.Coker.Web.Public.Views.Shared.Components.SideFloating
 
         private readonly IHtmlContentAppService htmlContentAppService;
         private readonly IConfiguration Configuration;
+        private readonly IWebsiteApplication websiteApplication;
         public SideFloating(
             IHtmlContentAppService htmlContentAppService,
-            IConfiguration Configuration
+            IConfiguration Configuration,
+            IWebsiteApplication websiteApplication
             )
         {
             this.htmlContentAppService = htmlContentAppService;
             this.Configuration = Configuration;
+            this.websiteApplication = websiteApplication;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var siteId = Configuration.GetValue<long>("WebConfig:SiteId");
-            var rightSideAd = JsonConvert.DeserializeObject<List<HtmlContentDisplayDto>>(JsonConvert.SerializeObject((await htmlContentAppService.GetDisplay(siteId, 12, 4)).Value));
+            var website = HttpContext.GetRouteData().Values["website"];
+            if (website == null)
+            {
+                website = HttpContext.GetRouteData().Values["key"];
+            }
+            var website_str = website == null ? "" : website.ToString();
+            var defaultData = await websiteApplication.GetDefaultData(siteId, website_str);
+            var rightSideAd = JsonConvert.DeserializeObject<List<HtmlContentDisplayDto>>(JsonConvert.SerializeObject((await htmlContentAppService.GetDisplay(defaultData.Id, 12, 4)).Value));
             SideFloatingViewModel model = new SideFloatingViewModel
             {
                 rightSideAd = rightSideAd

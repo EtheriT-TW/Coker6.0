04662867b3104d9cf20e8962db19a7f4d8296283
charles1984s <charles@ether.com.tw>
2023-06-28 11:23:01 +0800

    [update] 標籤匯入判斷

--- src/EtheriT.Coker.Web.ConsoleApp/Controllers/NewDataApplicaation.cs
+++ src/EtheriT.Coker.Web.ConsoleApp/Controllers/NewDataApplicaation.cs
@@ -10,13 +10,47 @@ namespace EtheriT.Coker.Web.ConsoleApp.Controllers
     public class NewDataApplicaation
     {
         private string newDb { get; set; }
-        public NewDataApplicaation(string connectionStr) {
+        public NewDataApplicaation(string connectionStr)
+        {
             newDb = connectionStr;
         }
-        public bool saveData(List<Article> articles) {
-            using (var dbContext = new NewDbContext(newDb)) {
+        public bool saveData(List<Article> articles)
+        {
+            using (var dbContext = new NewDbContext(newDb))
+            {
                 dbContext.AddRange(articles);
                 dbContext.SaveChanges();
+
+                var ArtTags = dbContext.Articles.GroupBy(e => e.Description).Select(e => e.Key).ToList();
+                if (ArtTags.FindIndex(e => e == "全部") >= 0)
+                {
+                    ArtTags[ArtTags.FindIndex(e => e == "全部")] = "園區廠商";
+                }
+
+                var TotalTags = dbContext.Tags.Select(e => e.Title).ToList();
+                List<string?> addTagStringList = ArtTags.FindAll(e => !TotalTags.Contains(e ?? ""));
+                List<Tag> addTagsList = new List<Tag>();
+
+                for (int i = 0; i < addTagStringList.Count; i++)
+                {
+                    var tag = addTagStringList[i];
+                    if (tag != null)
+                    {
+                        addTagsList.Add(new Tag
+                        {
+                            Title = tag,
+                            CreationTime = DateTime.Now,
+                            CreatorUserId = 1,
+                            IsDeleted = false,
+                            FK_WebsiteId = articles[0].FK_WebsiteId
+                        });
+                    }
+                }
+                if (addTagsList.Count > 0)
+                {
+                    dbContext.AddRange(addTagsList);
+                    dbContext.SaveChanges();
+                }
             }
             return true;
         }

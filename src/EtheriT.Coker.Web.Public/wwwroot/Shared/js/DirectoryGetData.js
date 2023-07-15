f0db8ca3d68752348b78743f5fa82aafe1b8571c
PeelSummer <m1020117@gmail.com>
2023-07-15 18:11:11 +0800

    [update] 目錄右側元件(可同步更新)

--- src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/DirectoryGetData.js
+++ src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/DirectoryGetData.js
@@ -32,37 +32,41 @@ function DirectoryGetDataInit() {
     })
     $(".menu_directory").each(function () {
         var $self = $(this);
-        console.log($self)
-        console.log($self.data("dirid"))
-        if ($self.data("init") != true && $self.data("dirid") > 0) {
-            Directory.getDirectoryMenuData({ Id: $self.data("dirid"), WebsiteId: typeof (SiteId) != "undefined" ? SiteId : 0 }).done(function (result) {
-                $self.find(".title").text(result.title)
-                $.each(result.children, function (index, SecIItem) {
-                    $self.find('.title').text(result.title);
-                    if (SecIItem.children != null) {
-                        var item = $($("#TemplateAccordionItem").html()).clone();
-                        item.find(".sectitle").text(SecIItem.title);
-                        item.find(".accordion-header").attr("id", `secheader${SecIItem.Id}`);
-                        item.find(".accordion-collapse").attr({
-                            "aria-labelledby": `secheader${SecIItem.Id}`,
-                            "id": `seccollapse${SecIItem.Id}`
-                        });
-                        item.find(".accordion-button").attr({
-                            "data-bs-target": `#seccollapse${SecIItem.Id}`,
-                            "aria-controls": `seccollapse${SecIItem.Id}`,
-                        });
-                        var $body = item.find(".accordion-body");
-                        $.each(SecIItem.children, function (index, ThirdIItem) {
-                            $body.append(`<a href="${ThirdIItem.routerName}" title="連結至：${ThirdIItem.title}" class="list-group-item list-group-item-action border-0 py-3">${ThirdIItem.title}</a>`)
-                        })
-                        $self.find(".accordion").append(item);
-                    } else {
-                        var html = `<div class="accordion-item border-0 border-bottom px-1"><a href="${SecIItem.routerName}" title="連結至：${SecIItem.title}" class="list-group-item border-0 py-3 custom_h5 text-black">${SecIItem.title}</a></div>`
-                        $self.find(".accordion").append(html);
-                    }
-                })
+        var dirid = $self.data("dirid") > 0 ? $self.data("dirid") : $self.attr("data-dirid") > 0 ? $self.attr("data-dirid") : 0;
+
+        if (typeof ($self.data("prevdirid")) == "undefined" || dirid != $self.data("prevdirid")) {
+            $self.data("prevdirid", dirid);
+            $self.find(".title").text("");
+            $self.find(".accordion").empty();
+            Directory.getDirectoryMenuData({ Id: dirid, WebsiteId: typeof (SiteId) != "undefined" ? SiteId : 0 }).done(function (result) {
+                if (typeof (result) != "undefined") {
+                    $self.find(".title").text(result.title)
+                    $.each(result.children, function (index, SecIItem) {
+                        $self.find('.title').text(result.title);
+                        if (SecIItem.children != null) {
+                            var item = $($("#TemplateAccordionItem").html()).clone();
+                            item.find(".sectitle").text(SecIItem.title);
+                            item.find(".accordion-header").attr("id", `secheader${SecIItem.Id}`);
+                            item.find(".accordion-collapse").attr({
+                                "aria-labelledby": `secheader${SecIItem.Id}`,
+                                "id": `seccollapse${SecIItem.Id}`
+                            });
+                            item.find(".accordion-button").attr({
+                                "data-bs-target": `#seccollapse${SecIItem.Id}`,
+                                "aria-controls": `seccollapse${SecIItem.Id}`,
+                            });
+                            var $body = item.find(".accordion-body");
+                            $.each(SecIItem.children, function (index, ThirdIItem) {
+                                $body.append(`<a href="${ThirdIItem.routerName}" title="連結至：${ThirdIItem.title}" class="list-group-item list-group-item-action border-0 py-3">${ThirdIItem.title}</a>`)
+                            })
+                            $self.find(".accordion").append(item);
+                        } else {
+                            var html = `<div class="accordion-item border-0 border-bottom px-1"><a href="${SecIItem.routerName}" title="連結至：${SecIItem.title}" class="list-group-item border-0 py-3 custom_h5 text-black">${SecIItem.title}</a></div>`
+                            $self.find(".accordion").append(html);
+                        }
+                    })
+                }
             });
-            $self.data("init", true);
         }
     })
 }

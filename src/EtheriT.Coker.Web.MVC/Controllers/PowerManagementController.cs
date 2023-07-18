5b9ec128e3564a138c20ca1a08750ffd71b0be9f
babupersonal <asdfgh535060@gmail.com>
2023-07-18 17:31:19 +0800

    [update] 登入密碼可視化、忘記密碼介面排版、個人資料修改介面調整、權限控制頁面排版

---
+++ src/EtheriT.Coker.Web.MVC/Controllers/PowerManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EtheriT.Coker.Web.MVC.Controllers
+{
+	public class PowerManagementController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}

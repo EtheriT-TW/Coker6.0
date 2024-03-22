e567f2ac09a9871f6b2289abf0c27401abbe6bba
bronson113 <bronson1013@gmail.com>
2024-03-22 16:05:12 +0800

    [Update] 內嵌pdf-viewer

--- src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/FlipBook.js
+++ src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/FlipBook.js
@@ -8,12 +8,27 @@
 }
 
     var target_pdf = $this.data('pdf');
-    console.log(target_pdf)
+    console.log(target_pdf);
 
-    var flipbook_container = jQuery('<iframe>');
-    flipbook_container.attr('src', '/lib/pdf-viewer/external/pdfjs-2.1.266-dist/web/viewer.html?file=' + encodeURIComponent(target_pdf));
+    var flipbook_container = document.createElement("div");
+
+    const url = new URL(window.location.href);
+    url.searchParams.set('file', target_pdf);
+    window.history.replaceState(null, null, url);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) { flipbook_container.innerHTML = this.responseText; }
+            if (this.status == 404) { flipbook_container.innerHTML = "Page not found."; }
     $this.append(flipbook_container);
     $this.removeClass("d-none");
+            console.log("show pdf");
+        }
+    }
+    xhttp.open("GET", '/lib/pdf-viewer/external/pdfjs-2.1.266-dist/web/viewer.html?file=' + encodeURIComponent(target_pdf), true);
+    xhttp.send();
+
 
 }
 

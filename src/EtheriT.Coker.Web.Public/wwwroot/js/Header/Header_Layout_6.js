88a043c5777b05d0353259e815f8f796c7407450
PeelSummer <m1020117@gmail.com>
2024-08-14 22:01:57 +0800

    [update]調整版頭於內頁顯示

--- src/EtheriT.Coker.Web.Public/wwwroot/js/Header/Header_Layout_6.js
+++ src/EtheriT.Coker.Web.Public/wwwroot/js/Header/Header_Layout_6.js
@@ -4,8 +4,6 @@ function HeaderInit() {
     var backgroundimgurl = "";
 
     if ($('body').hasClass('home')) {
-        $(".titleimage").removeClass("d-none");
-        $(".headernews").removeClass("d-none");
         $("link").each(function (index) {
             if (typeof ($(this).attr("data-orgname")) == "string") {
                 backgroundimgurl = `url("/upload/${$(this).attr("data-orgname")}/backgroundtop.jpg")`;

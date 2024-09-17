7191dd4b876f19441e98489b2a1f072825b34b6f
PeelSummer <m1020117@gmail.com>
2024-09-17 14:20:51 +0800

    [update]廣告點擊、顯示事件

---
+++ src/EtheriT.Coker.Web.Public/Controllers/api/AdvertiseController.cs
@@ -0,0 +1,32 @@
+﻿using EtheriT.Coker.Application.Dto;
+using EtheriT.Coker.Application.Shared.Advertise;
+using EtheriT.Coker.Application.Shared.Dto.Advertise;
+using EtheriT.Coker.Application.Shared.Dto.enumType;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EtheriT.Coker.Web.Public.Controllers.api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AdvertiseController : Controller
+    {
+
+        private readonly IAdvertiseAppService advertiseAppService;
+        public AdvertiseController(IAdvertiseAppService advertiseAppService)
+        {
+            this.advertiseAppService = advertiseAppService;
+        }
+        [HttpPost]
+        public async Task<ResponseMessageDto> ActivityClick(AdvertiseLogDto dto)
+        {
+            dto.Action = (int)LogActionEnum.點擊;
+            return await advertiseAppService.ActivityLog(dto);
+        }
+        [HttpPost]
+        public async Task<ResponseMessageDto> ActivityExposure(AdvertiseLogDto dto)
+        {
+            dto.Action = (int)LogActionEnum.顯示;
+            return await advertiseAppService.ActivityLog(dto);
+        }
+    }
+}

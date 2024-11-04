03b411c954ac85e3741d0474eaabef2c2e2ecc44
user <a125585@yahoo.com.tw>
2024-11-04 18:28:18 +0800

    [add] 後台新增會員管理選單

---
+++ src/EtheriT.Coker.Web.MVC/Views/MemberManagement/UserType.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtheriT.Coker.Web.MVC.Views.MemberManagement
+{
+    public class UserTypeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

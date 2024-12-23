5588dd7c15df02268cd3b5bbef8b981b6012d244
user <a125585@yahoo.com.tw>
2024-12-23 16:14:46 +0800

    [update] 解決資策會無障礙HM1240402E問題

--- src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/DirectoryGetData.js
+++ src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/DirectoryGetData.js
@@ -381,7 +381,8 @@ function DirectoryDataInsert($item, result) {
         content.find(".title").text(data.title);
         content.find(".subtitle").text(data.subtitle);
         content.find(".description").html(data.description);
-        if (content.find("a img").length && content.find("a").find("h3,h4,h5,h6").length) {
+        //符合無障礙規範，若a裡有圖片或文字則alt為空
+        if (content.find("img").length && content.find("h3,h4,h5,h6,span,p").length) {
             content.find("img").imgCheck().attr("alt", "");
         }
         if (content.find(".location").length > 0 && (data.location == null || data.location == "")) content.find(".location").parents(".py-2").remove();

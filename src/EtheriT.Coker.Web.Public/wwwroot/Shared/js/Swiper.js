47eed7e03dd5c1d00a1ec82374a05d519cc10cdc
charles1984s <charles@ether.com.tw>
2024-12-24 13:49:51 +0800

    [update] 調整輪播元件內focus錯誤問題調整

--- src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/Swiper.js
+++ src/EtheriT.Coker.Web.Public/wwwroot/Shared/js/Swiper.js
@@ -103,8 +103,9 @@ function SwiperInit(obj) {
             }
             thisSwiper = $(this);
             $(this).off("mouseover").on("mouseover", stop);
-            $(this).find(".swiper-slide a").off("focus").on("focus", function () {
-                const activeIndex = $(swiper.el).find(":focus").parents(".swiper-slide").attr("aria-label").split(" / ")[0];
+            $(this).find(".swiper-slide a,.swiper-slide").off("focus").on("focus", function () {
+                const $item = $(this).hasClass("swiper-slide") ? $(this) : $(this).parents(".swiper-slide");
+                const activeIndex = $item.attr("aria-label").split(" / ")[0];
                 swiper.slideTo(activeIndex - 1, 300);
                 stop();
             });

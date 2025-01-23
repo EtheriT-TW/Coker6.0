5ba4d88b56229a45bbc875e4a44665cc1b9f31de
PeelSummer <m1020117@gmail.com>
2025-01-23 10:27:56 +0800

    [update]PaymentType新增金額上下限、Icon欄位

--- src/EtheriT.Coker.Core/Models/PaymentType.cs
+++ src/EtheriT.Coker.Core/Models/PaymentType.cs
@@ -9,6 +9,9 @@ namespace EtheriT.Coker.Core.Models
         [StringLength(50)] public string? Code { get; set; }
         public bool Used { get; set; }
         public int SerNo { get; set; }
+        public int MinAmount { get; set; } = 1;
+        public int? MaxAmount { get; set; }
+        public string Icons { get; set; }
         public long FK_ThirdPartyId { get; set; }
         public ThirdParty? ThirdParty { get; set; }
         public List<LogisticsType_PaymentType>? LogisticsType_Payments { get; set; }

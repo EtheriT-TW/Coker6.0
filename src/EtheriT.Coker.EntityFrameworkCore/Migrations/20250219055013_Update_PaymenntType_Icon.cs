36e56e746a7a5d2a2688df02b043e889df1e8281
charles1984s <charles@ether.com.tw>
2025-02-19 13:52:04 +0800

    [update] 建立PaymentType Icon更新的migration

---
+++ src/EtheriT.Coker.EntityFrameworkCore/Migrations/20250219055013_Update_PaymenntType_Icon.cs
@@ -0,0 +1,309 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EtheriT.Coker.EntityFrameworkCore.Migrations
+{
+    public partial class Update_PaymenntType_Icon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Icons",
+                value: "pay05.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Icons",
+                value: "pay05.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Icons",
+                value: "pay02.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "Icons",
+                value: "pay06_支付連銀行.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "Icons",
+                value: "pay03.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "Icons",
+                value: "pay04.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 11L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 12L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 13L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 14L,
+                column: "Icons",
+                value: "pay01.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 15L,
+                column: "Icons",
+                value: "pay_07.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 16L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 17L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 18L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 19L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 20L,
+                column: "Icons",
+                value: "pay_08_信用卡.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 21L,
+                column: "Icons",
+                value: "pay05.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 22L,
+                column: "Icons",
+                value: "pay_07.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 24L,
+                column: "Icons",
+                value: "pay04.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 26L,
+                column: "Icons",
+                value: "pay03.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 11L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 12L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 13L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 14L,
+                column: "Icons",
+                value: "LINEPAY.png");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 15L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 16L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 17L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 18L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 19L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 20L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 21L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 22L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 24L,
+                column: "Icons",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "PaymentTypes",
+                keyColumn: "Id",
+                keyValue: 26L,
+                column: "Icons",
+                value: "");
+        }
+    }
+}

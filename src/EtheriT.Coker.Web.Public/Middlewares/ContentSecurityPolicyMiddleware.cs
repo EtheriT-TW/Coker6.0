52aff4d1f10c07f6ec769d7814aec1813ad5772f
PeelSummer <m1020117@gmail.com>
2025-03-04 11:41:32 +0800

    [update]更正騰訊白名單

--- src/EtheriT.Coker.Web.Public/Middlewares/ContentSecurityPolicyMiddleware.cs
+++ src/EtheriT.Coker.Web.Public/Middlewares/ContentSecurityPolicyMiddleware.cs
@@ -45,11 +45,11 @@ namespace EtheriT.Coker.Web.Public.Middlewares
                 // google 翻譯 script-src、style-src要加上 'unsafe-inline' 目前還找不到解決方案 
                 context.Response.Headers["Content-Security-Policy"] =
                     $"default-src 'self';" +
-                    $"script-src 'self' '{selfInline}' *.google.com *.googletagmanager.com *.googleadservices.com *.googleapis.com *.facebook.net *.jquery.com *.yimg.com *.google-analytics.com scaleflex.cloudimg.io googleads.g.doubleclick.net d.line-scdn.net cdn.ckeditor.com remotejs.com www.instagram.com v.qq.com; " +
+                    $"script-src 'self' '{selfInline}' *.google.com *.googletagmanager.com *.googleadservices.com *.googleapis.com *.facebook.net *.jquery.com *.yimg.com *.google-analytics.com scaleflex.cloudimg.io googleads.g.doubleclick.net d.line-scdn.net cdn.ckeditor.com remotejs.com www.instagram.com; " +
                     $"style-src 'self' '{selfInline}' *.googleapis.com  *.google.com *.gstatic.com cdnjs.cloudflare.com cdn.ckeditor.com; " +
                     $"font-src 'self' data: fonts.gstatic.com cdnjs.cloudflare.com; " +
                     $"img-src 'self' *.ezsale.tw *.facebook.com *.yahoo.com *.google.com *.google.com.tw *.google-analytics.com *.googletagmanager.com *.gstatic.com *.googleapis.com *.youtube.com i.ytimg.com ad.doubleclick.net googleads.g.doubleclick.net tr.line.me cdn.ckeditor.com data: blob:; " +
-                    $"frame-src 'self' *.ezsale.tw *.google.com *.youtube.com *.youtube-nocookie.com *.facebook.com *.instagram.com *.googletagmanager.com *.doubleclick.net;" +
+                    $"frame-src 'self' *.ezsale.tw *.google.com *.youtube.com *.youtube-nocookie.com *.facebook.com *.instagram.com *.googletagmanager.com *.doubleclick.net v.qq.com;" +
                     $"connect-src {connectSrc}" +
                     $"frame-ancestors 'self' *.ezsale.tw;";
                 //cache 限制設定

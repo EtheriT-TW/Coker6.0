da52128c4c46edd2285489ac7b97927020cb1420
PeelSummer <m1020117@gmail.com>
2025-03-11 11:07:53 +0800

    [update]滿額折抵Table建立

---
+++ src/EtheriT.Coker.Core/Models/Marketing.cs
@@ -0,0 +1,25 @@
+﻿using EtheriT.Coker.Core.Entity;
+
+namespace EtheriT.Coker.Core.Models
+{
+    // 行銷活動
+    public class Marketing : FullAuditedEntity
+    {
+        // 是否開啟
+        public bool Enable { get; set; }
+        // 折抵目標(滿額、滿幾件)
+        public int Target {  get; set; }
+        // 折扣類型(金額扣抵、打折......)
+        public int DiscountType { get; set; }
+        // 折扣值
+        public double Discount { get; set; }
+        // 最高折抵
+        public double? MaxDiscount { get; set; }
+        // 起始時間
+        public virtual DateTime? StartTime { get; set; }
+        // 結束時間
+        public virtual DateTime? EndTime { get; set; }
+        // 是否永久顯示
+        public bool Permanent { get; set; }
+    }
+}
---
+++ src/EtheriT.Coker.EntityFrameworkCore/Migrations/20250311030516_Added_Table_Marketing.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EtheriT.Coker.EntityFrameworkCore.Migrations
+{
+    public partial class Added_Table_Marketing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marketing",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Enable = table.Column<bool>(type: "bit", nullable: false),
+                    Target = table.Column<int>(type: "int", nullable: false),
+                    DiscountType = table.Column<int>(type: "int", nullable: false),
+                    Discount = table.Column<double>(type: "float", nullable: false),
+                    MaxDiscount = table.Column<double>(type: "float", nullable: true),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Permanent = table.Column<bool>(type: "bit", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: false),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marketing", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marketing");
+        }
+    }
+}

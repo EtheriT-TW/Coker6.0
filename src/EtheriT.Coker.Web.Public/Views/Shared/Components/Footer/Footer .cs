f45aaf6404d010d5e1bdfe00afff5f7cf3a9cf8b
PeelSummer <m1020117@gmail.com>
2025-03-14 14:15:11 +0800

    [update]沅哲Footer修改、css微調

--- src/EtheriT.Coker.Web.Public/Views/Shared/Components/Footer/Footer .cs
+++ src/EtheriT.Coker.Web.Public/Views/Shared/Components/Footer/Footer .cs
@@ -107,7 +107,7 @@ namespace EtheriT.Coker.Web.Public.Views.Shared.Components.Footer
                             footerViewModel = new FooterViewModel
                             {
                                 footerViewModels = new List<FooterViewModel> {
-                                    new FooterViewModel { Title = "夯酷客", Link = "", footerViewModels = new List<FooterViewModel> {
+                                    new FooterViewModel { Title = "夯酷客(HomeCook)", Link = "", footerViewModels = new List<FooterViewModel> {
                                             new FooterViewModel { Title = "香辛與調味料", Link = "/yuanjer/Flavorings" },
                                             new FooterViewModel { Title = "高湯鮮味", Link = "/yuanjer/umami" },
                                             new FooterViewModel { Title = "炸粉", Link = "/yuanjer/batter" },
@@ -143,7 +143,7 @@ namespace EtheriT.Coker.Web.Public.Views.Shared.Components.Footer
                                 Content = new List<string>
                                 {
                                     "Copyright©",
-                                    "YUAN JER CO., LTD",
+                                    "沅哲(Yunjer) 有限公司",
                                     "版權所有"
                                 }
                             };

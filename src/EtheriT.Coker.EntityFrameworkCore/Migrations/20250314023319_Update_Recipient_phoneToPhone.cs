b5f1adf5477ef8eb52f10e11c1511ef0d9aa30e7
PeelSummer <m1020117@gmail.com>
2025-03-17 11:14:48 +0800

    [update]Migration補傳

---
+++ src/EtheriT.Coker.EntityFrameworkCore/Migrations/20250314023319_Update_Recipient_phoneToPhone.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EtheriT.Coker.EntityFrameworkCore.Migrations
+{
+    public partial class Update_Recipient_phoneToPhone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Telephone",
+                table: "Recipients",
+                newName: "TelePhone");
+
+            migrationBuilder.RenameColumn(
+                name: "Cellphone",
+                table: "Recipients",
+                newName: "CellPhone");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipientTelephone",
+                table: "Order_Headers",
+                newName: "RecipientTelePhone");
+
+            migrationBuilder.RenameColumn(
+                name: "OrdererTelephone",
+                table: "Order_Headers",
+                newName: "OrdererTelePhone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TelePhone",
+                table: "Recipients",
+                newName: "Telephone");
+
+            migrationBuilder.RenameColumn(
+                name: "CellPhone",
+                table: "Recipients",
+                newName: "Cellphone");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipientTelePhone",
+                table: "Order_Headers",
+                newName: "RecipientTelephone");
+
+            migrationBuilder.RenameColumn(
+                name: "OrdererTelePhone",
+                table: "Order_Headers",
+                newName: "OrdererTelephone");
+        }
+    }
+}

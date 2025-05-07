06f7c3b92e86b4aa57d55d1bcfafa209368131b6
PeelSummer <m1020117@gmail.com>
2025-05-07 17:07:09 +0800

    [update]ECPay Dto 名稱修正

--- src/EtheriT.Coker.Application.Shared/ThirdParty/IECPayAppService.cs
+++ src/EtheriT.Coker.Application.Shared/ThirdParty/IECPayAppService.cs
@@ -9,7 +9,7 @@ namespace EtheriT.Coker.Application.Shared.ThirdParty
         public Task<ResponseMessageDto> ECPayOrderState(long ohid);
         public Task<ResponseMessageDto> ECPayRefund(long ohid);
         public Task<IActionResult> ECPayOrderResult(string ResultData);
-        public Task<String> ECPayReturn(ECPayResponseDto ResultResponseData);
+        public Task<String> ECPayReturn(ECPayReturnResponseDto ResultResponseData);
         public Task<ResponseMessageDto> ECPayCreatePayment(ECPayPaymentInfoDto PaymentInfo);
         public Task<ResponseMessageDto> ECPayGetToken(long ohid);
     }

aa241379401049b8859db5c2451f0d02ab8cabe0
Jason <jokkson@gmail.com>
2025-05-08 21:31:15 +0800

    [新增] 加入 Dto 類別讓紅利設定使用

---
+++ src/EtheriT.Coker.Application.Shared/Dto/BonusManagement/CreateOrUpdateSettingsDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtheriT.Coker.Application.Shared.Dto.BonusManagement
+{
+    public class CreateOrUpdateSettingsDto
+    {
+        /// <summary>
+        /// 迎新禮 (加入會員贈送紅利的點數)
+        /// </summary>
+        public int SignupBonusPoints { get; set; }
+
+        /// <summary>
+        /// 紅利扣抵條件 (消費滿X元可啟動紅利扣抵)
+        /// </summary>
+        public decimal MinOrderForRedemption { get; set; }
+
+        /// <summary>
+        /// 最高抵扣% (達到啟動紅利扣抵條件後，每次最高抵扣%)
+        /// </summary>
+        public decimal MaxRedemptionPercent { get; set; }
+
+        /// <summary>
+        /// 消費滿額條件 (當次消費滿額X元可啟動獲得數紅利)
+        /// </summary>
+        public decimal MinOrderForEarnPoints { get; set; }
+
+        /// <summary>
+        /// 獲得%數紅利 (消費滿額時，可獲得%數紅利)
+        /// </summary>
+        public decimal RewardRatePercent { get; set; }
+
+        /// <summary>
+        /// 紅利有效天數
+        /// </summary>
+        public int RewardPointsExpireDays { get; set; }
+    }
+}
---
+++ src/EtheriT.Coker.Web.MVC/Controllers/api/BonusManagementController.cs
@@ -0,0 +1,26 @@
+﻿using EtheriT.Coker.Application.Dto;
+using EtheriT.Coker.Application.Company;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using EtheriT.Coker.Application.Shared.BonusManagement;
+using EtheriT.Coker.Application.Shared.Dto.BonusManagement;
+
+namespace EtheriT.Coker.Web.MVC.Controllers.api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class BonusManagementController : Controller, IBonusManagementAppService
+    {
+        private readonly IBonusManagementAppService _bonusManagementAppService;
+        public BonusManagementController(IBonusManagementAppService bonusManagementAppService)
+        {
+            _bonusManagementAppService = bonusManagementAppService;
+        }
+
+        public async Task<GetBonusSettingForEditOutput> GetBonusSettingForEdit()
+        {
+            return await _bonusManagementAppService.GetBonusSettingForEdit();
+        }
+    }
+}

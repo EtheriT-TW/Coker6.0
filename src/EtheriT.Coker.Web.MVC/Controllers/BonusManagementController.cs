6812b0a3639ca8858e7befaee9fb80730f569fe8
Jason <jokkson@gmail.com>
2025-05-08 21:31:38 +0800

    [修改] 更新紅利設定表單，新增數值限制與格式化功能

--- src/EtheriT.Coker.Web.MVC/Controllers/BonusManagementController.cs
+++ src/EtheriT.Coker.Web.MVC/Controllers/BonusManagementController.cs
@@ -1,4 +1,5 @@
 ﻿using EtheriT.Coker.Application.StoreSet;
+using EtheriT.Coker.Web.MVC.Models.BonusManagement;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EtheriT.Coker.Web.MVC.Controllers
@@ -11,10 +12,15 @@ namespace EtheriT.Coker.Web.MVC.Controllers
         }
         public async Task<IActionResult> Settings()
         {
-            var response = await _storeSetAppService.getAll(new List<long> { 6 });
-            response.StoreSetGroupId = 6;
-            ViewData["Title"] = "BonusSettings";
-            return View("~/Views/SystemManagement/SEO.cshtml", response);
+            var bonusSettingData = await _storeSetAppService.getAll(new List<long> { 6 });
+
+
+            //response.StoreSetGroupId = 6;
+            //ViewData["Title"] = "BonusSettings";
+            //return View("~/Views/SystemManagement/SEO.cshtml", response);
+
+            SettingsModel model = new SettingsModel();
+            return View(model);
         }
         public IActionResult Transaction()
         {
---
+++ src/EtheriT.Coker.Web.MVC/Models/BonusManagement/SettingsModel.cs
@@ -0,0 +1,8 @@
+﻿using EtheriT.Coker.Application.Shared.Dto.BonusManagement;
+
+namespace EtheriT.Coker.Web.MVC.Models.BonusManagement
+{
+    public class SettingsModel : GetBonusSettingForEditOutput
+    {
+    }
+}

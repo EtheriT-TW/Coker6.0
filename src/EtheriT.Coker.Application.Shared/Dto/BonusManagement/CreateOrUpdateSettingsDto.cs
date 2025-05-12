843dc7038ed56ab4597b1b5c9c99fb1ac7a0f93e
Jason <jokkson@gmail.com>
2025-05-12 16:34:10 +0800

    [修改] 更新 CreateOrUpdateSettingsDto，將屬性類型改為可為 null

--- src/EtheriT.Coker.Application.Shared/Dto/BonusManagement/CreateOrUpdateSettingsDto.cs
+++ src/EtheriT.Coker.Application.Shared/Dto/BonusManagement/CreateOrUpdateSettingsDto.cs
@@ -11,31 +11,31 @@ namespace EtheriT.Coker.Application.Shared.Dto.BonusManagement
         /// <summary>
         /// 迎新禮 (加入會員贈送紅利的點數)
         /// </summary>
-        public int SignupBonusPoints { get; set; }
+        public int? SignupBonusPoints { get; set; }
 
         /// <summary>
         /// 紅利扣抵條件 (消費滿X元可啟動紅利扣抵)
         /// </summary>
-        public decimal MinOrderForRedemption { get; set; }
+        public decimal? MinOrderForRedemption { get; set; }
 
         /// <summary>
         /// 最高抵扣% (達到啟動紅利扣抵條件後，每次最高抵扣%)
         /// </summary>
-        public decimal MaxRedemptionPercent { get; set; }
+        public decimal? MaxRedemptionPercent { get; set; }
 
         /// <summary>
         /// 消費滿額條件 (當次消費滿額X元可啟動獲得數紅利)
         /// </summary>
-        public decimal MinOrderForEarnPoints { get; set; }
+        public decimal? MinOrderForEarnPoints { get; set; }
 
         /// <summary>
         /// 獲得%數紅利 (消費滿額時，可獲得%數紅利)
         /// </summary>
-        public decimal RewardRatePercent { get; set; }
+        public decimal? RewardRatePercent { get; set; }
 
         /// <summary>
         /// 紅利有效天數
         /// </summary>
-        public int RewardPointsExpireDays { get; set; }
+        public int? RewardPointsExpireDays { get; set; }
     }
 }

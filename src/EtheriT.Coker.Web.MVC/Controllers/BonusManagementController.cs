ff2420394e899e8f0971dc991823d68ca7decd4e
Jason <jokkson@gmail.com>
2025-05-12 16:34:54 +0800

    [修改] 更新 BonusManagementController，新增取得紅利設定說明功能並調整建構函式

--- src/EtheriT.Coker.Web.MVC/Controllers/BonusManagementController.cs
+++ src/EtheriT.Coker.Web.MVC/Controllers/BonusManagementController.cs
@@ -1,4 +1,8 @@
-﻿using EtheriT.Coker.Application.StoreSet;
+﻿using AutoMapper;
+using DevExpress.ClipboardSource.SpreadsheetML;
+using DevExpress.CodeParser;
+using EtheriT.Coker.Application.Shared.BonusManagement;
+using EtheriT.Coker.Application.StoreSet;
 using EtheriT.Coker.Web.MVC.Models.BonusManagement;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,20 +10,38 @@ namespace EtheriT.Coker.Web.MVC.Controllers
 {
     public class BonusManagementController : Controller
     {
-        private readonly IStoreSetAppService _storeSetAppService;
-        public BonusManagementController(IStoreSetAppService storeSetAppService) {
-            _storeSetAppService = storeSetAppService;
+        private readonly IBonusManagementAppService _bonusManagementAppService;
+        private readonly IMapper mapper;
+        public BonusManagementController(IBonusManagementAppService bonusManagementAppService,
+                                         IMapper mapper)
+        {
+            _bonusManagementAppService = bonusManagementAppService;
+            this.mapper = mapper;
         }
         public async Task<IActionResult> Settings()
         {
-            var bonusSettingData = await _storeSetAppService.getAll(new List<long> { 6 });
+            var bonusSettingData = await _bonusManagementAppService.GetBonusSettingForEdit();
+            var bonusSettingHelpText = await _bonusManagementAppService.GetBonusSettingHelpTextForEdit();
 
+            SettingsModel model = new SettingsModel()
+            {
+                // 取得紅利設定
+                SiteId = bonusSettingData.SiteId,
+                SignupBonusPoints = bonusSettingData.SignupBonusPoints,
+                MinOrderForRedemption = bonusSettingData.MinOrderForRedemption,
+                MaxRedemptionPercent = bonusSettingData.MaxRedemptionPercent,
+                MinOrderForEarnPoints = bonusSettingData.MinOrderForEarnPoints,
+                RewardRatePercent = bonusSettingData.RewardRatePercent,
+                RewardPointsExpireDays = bonusSettingData.RewardPointsExpireDays,
 
-            //response.StoreSetGroupId = 6;
-            //ViewData["Title"] = "BonusSettings";
-            //return View("~/Views/SystemManagement/SEO.cshtml", response);
-
-            SettingsModel model = new SettingsModel();
+                // 取得紅利設定說明
+                SignupBonusPointsHelpText = bonusSettingHelpText.SignupBonusPointsHelpText,
+                MinOrderForRedemptionHelpText = bonusSettingHelpText.MinOrderForRedemptionHelpText,
+                MaxRedemptionPercentHelpText = bonusSettingHelpText.MaxRedemptionPercentHelpText,
+                MinOrderForEarnPointsHelpText = bonusSettingHelpText.MinOrderForEarnPointsHelpText,
+                RewardRatePercentHelpText = bonusSettingHelpText.RewardRatePercentHelpText,
+                RewardPointsExpireDaysHelpText = bonusSettingHelpText.RewardPointsExpireDaysHelpText
+            };
             return View(model);
         }
         public IActionResult Transaction()
--- src/EtheriT.Coker.Web.MVC/Controllers/api/BonusManagementController.cs
+++ src/EtheriT.Coker.Web.MVC/Controllers/api/BonusManagementController.cs
@@ -9,8 +9,8 @@ namespace EtheriT.Coker.Web.MVC.Controllers.api
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    [Authorize]
-    public class BonusManagementController : Controller, IBonusManagementAppService
+    //[Authorize]
+    public class BonusManagementController : Controller
     {
         private readonly IBonusManagementAppService _bonusManagementAppService;
         public BonusManagementController(IBonusManagementAppService bonusManagementAppService)
@@ -21,6 +21,31 @@ namespace EtheriT.Coker.Web.MVC.Controllers.api
         public async Task<GetBonusSettingForEditOutput> GetBonusSettingForEdit()
         {
             return await _bonusManagementAppService.GetBonusSettingForEdit();
+        }
+
+        public async Task<GetBonusSettingHelpTextForEditOutput> GetBonusSettingHelpTextForEdit()
+        {
+            return await _bonusManagementAppService.GetBonusSettingHelpTextForEdit();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save([FromBody] CreateOrUpdateSettingsDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                // 呼叫 Service 層方法來更新設定
+                await _bonusManagementAppService.Save(model);
+                return Ok(new { success = true, message = "設定已成功更新" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { success = false, message = ex.Message });
+            }
         }
     }
 }

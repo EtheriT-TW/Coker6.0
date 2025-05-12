1189fd95566df77f729dfd0a8713090a834e7c7b
Jason <jokkson@gmail.com>
2025-05-12 16:35:13 +0800

    [新增] 更新 SettingsModel 以新增幫助文字屬性，並在設定表單中顯示相應的幫助文字；新增 JavaScript 檔案以處理設定儲存功能

--- src/EtheriT.Coker.Web.MVC/Models/BonusManagement/SettingsModel.cs
+++ src/EtheriT.Coker.Web.MVC/Models/BonusManagement/SettingsModel.cs
@@ -4,5 +4,16 @@ namespace EtheriT.Coker.Web.MVC.Models.BonusManagement
 {
     public class SettingsModel : GetBonusSettingForEditOutput
     {
+        public string? SignupBonusPointsHelpText { get; set; }
+
+        public string? MinOrderForRedemptionHelpText { get; set; }
+
+        public string? MaxRedemptionPercentHelpText { get; set; }
+
+        public string? MinOrderForEarnPointsHelpText { get; set; }
+
+        public string? RewardRatePercentHelpText { get; set; }
+
+        public string? RewardPointsExpireDaysHelpText { get; set; }
     }
 }

1aa90c4446ac7d282b3e979b2807b7c2b1d74d84
Jason <jokkson@gmail.com>
2025-05-23 14:24:05 +0800

    [update] 更新 FileManagementController，新增檔案類型過濾邏輯以支援允許的副檔名

--- src/EtheriT.Coker.Web.MVC/Controllers/FileManagementController.cs
+++ src/EtheriT.Coker.Web.MVC/Controllers/FileManagementController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EtheriT.Coker.Web.MVC.Controllers
 {
     public class FileManagementController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public FileManagementController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            Models.FileManagement.IndexModel model = new Models.FileManagement.IndexModel();
+
+
+            model.AllowContentType = _configuration.GetSection("VirtualDirectory:FileAllow:Ext").Get<List<string>>() ?? new List<string>();            // 使用 FileExtensionContentTypeProvider 從 MIME 值反推取得副檔名
+            var provider = new FileExtensionContentTypeProvider();
+
+            // 從 MIME 反向尋找副檔名並只保留允許的副檔名
+            model.AllowFileExtension = provider.Mappings.Where(x => model.AllowContentType.Contains(x.Value))
+                                                        .Select(x => x.Key)
+                                                        .OrderBy(x => x)
+                                                        .Distinct()
+                                                        .ToList();
+
+            return View(model);
         }
     }
 }
---
+++ src/EtheriT.Coker.Web.MVC/Models/FileManagement/IndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EtheriT.Coker.Web.MVC.Models.FileManagement
+{
+    public class IndexModel
+    {
+        public List<string> AllowContentType { get; set; } = new List<string>();
+        public List<string> AllowFileExtension { get; set; } = new List<string>();
+    }
+}

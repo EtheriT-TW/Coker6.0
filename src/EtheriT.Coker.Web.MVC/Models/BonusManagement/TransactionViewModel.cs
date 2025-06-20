80d9976f7288a8af03ab7a7b148ba78db87bb855
Jason <jokkson@gmail.com>
2025-06-20 11:47:52 +0800

    [update] 更新Transaction.cshtml，新增紅利異動表單及會員選擇功能

---
+++ src/EtheriT.Coker.Web.MVC/Models/BonusManagement/TransactionViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace EtheriT.Coker.Web.MVC.Models.BonusManagement
+{
+    public class TransactionViewModel
+    {
+        public readonly List<TransactionOperationSelectBoxData> TransactionOperationSelectBoxDataSource =
+            new List<TransactionOperationSelectBoxData>()
+            {
+                new TransactionOperationSelectBoxData() { DisplayName = "新增", OperationValue = "+" },
+                new TransactionOperationSelectBoxData() { DisplayName = "扣除", OperationValue = "-" },
+            };
+        public int ConstTransactionReasonMaxLength { get; set; } = 25;
+        public int ConstTransactionPointMax { get; set; } = 9999;
+        public int? ConstRewardPointsExpireDays { get; set; }
+        public DateTime ConstRewardPointsExpireDateTime { get; set; }
+    }
+
+    public class  TransactionOperationSelectBoxData
+    {
+        public string? DisplayName { get; set; }
+        public string? OperationValue { get; set; }
+    }
+}

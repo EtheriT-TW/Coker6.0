d6ea988e3512fb906be3619e9b23cafe179c9cb6
Jason <jokkson@gmail.com>
2025-06-25 21:21:27 +0800

    [update] 新增CreateUserTransactionDto和GetFrontUserDto類別以支持用戶交易和前端用戶資料

---
+++ src/EtheriT.Coker.Application.Shared/Dto/BonusManagement/CreateUserTransactionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtheriT.Coker.Application.Shared.Dto.BonusManagement
+{
+    public class CreateUserTransactionDto
+    {
+        public List<Guid> MemberUUID { get; set; } = new List<Guid>();
+        public string? TransactionOperation { get; set; }
+        public int TransactionPoint { get; set; }
+        public string? TransactionReason { get; set; }
+    }
+}

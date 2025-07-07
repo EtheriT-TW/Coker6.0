0164211dac3918fb462231ef23fdfb4a87fb33d2
charles1984s <charles@ether.com.tw>
2025-07-08 00:24:03 +0800

    [update] 同步BonusLogs 欄位調整

---
+++ src/EtheriT.Coker.EntityFrameworkCore/Migrations/20250707161136_Alert_Table_BonusLog.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EtheriT.Coker.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alert_Table_BonusLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BonusId",
+                table: "bonusLogDetails");
+
+            migrationBuilder.DropColumn(
+                name: "BonusLogId",
+                table: "bonusLogDetails");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "BonusId",
+                table: "bonusLogDetails",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "BonusLogId",
+                table: "bonusLogDetails",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bonusLogDetails_BonusId",
+                table: "bonusLogDetails",
+                column: "BonusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bonusLogDetails_BonusLogId",
+                table: "bonusLogDetails",
+                column: "BonusLogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_bonusLogDetails_BonusLog_BonusLogId",
+                table: "bonusLogDetails",
+                column: "BonusLogId",
+                principalTable: "BonusLog",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_bonusLogDetails_Bonus_BonusId",
+                table: "bonusLogDetails",
+                column: "BonusId",
+                principalTable: "Bonus",
+                principalColumn: "Id");
+        }
+    }
+}

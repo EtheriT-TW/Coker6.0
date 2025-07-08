f5a5b6c1e5c2ad47adb5bb77c0bf4417e2ff5956
Jason <jokkson@gmail.com>
2025-07-08 21:05:06 +0800

    [update] 新增 TransactionMailTemplateModel 類別

---
+++ src/EtheriT.Coker.Application.Shared/Dto/BonusManagement/TransactionMailTemplateModelDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtheriT.Coker.Application.Shared.Dto.BonusManagement
+{
+    public class TransactionMailTemplateModelDto
+    {
+        public string? MemberName { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+        public long MemberId { get; set; }
+        public string? TransactionOperation { get; set; }
+        public string? TransactionOperationName { get; set; }
+        public int TransactionPoint { get; set; }
+        public string? TransactionDescription { get; set; }
+        public int TransactionBalance { get; set; }
+        public DateTime RewardPointsExpireDateTime { get; set; }
+        public string? WebSiteName { get; set; }
+        public string? WebSiteUrl { get; set; }
+    }
+}

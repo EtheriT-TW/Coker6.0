dd966a7d5d4b428ed50da296c26da6086194279c
Jason <jokkson@gmail.com>
2025-07-08 21:04:19 +0800

    [update] 新增 MailTemplateAppService 類別及其實作

---
+++ src/EtheriT.Coker.Application.Shared/Common/IMailTemplateAppService.cs
@@ -0,0 +1,15 @@
+﻿using EtheriT.Coker.Application.Shared.Dto.enumType;
+using EtheriT.Coker.Application.Shared.Dto.MailTemplate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtheriT.Coker.Application.Shared.Common
+{
+    public interface IMailTemplateAppService
+    {
+        public Task<List<MailTemplateResultDto>> GetTemplateRenderAsync(MailTemplateTypeEnum templateType, List<MailTemplateInputDto> input);
+    }
+}

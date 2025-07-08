2b912ee7e273bcbee12910e30b706a9fe289a578
Jason <jokkson@gmail.com>
2025-07-08 21:04:19 +0800

    [update] 新增 MailTemplateAppService 類別及其實作

---
+++ src/EtheriT.Coker.Application.Shared/Dto/MailTemplate/MailTemplateResultDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtheriT.Coker.Application.Shared.Dto.MailTemplate
+{
+    public class MailTemplateResultDto
+    {
+        public string? Key { get; set; }
+        public string? Body { get; set; }
+    }
+}

95be2a1769c24f47e29936c6222088f99044f1f7
Jason <jokkson@gmail.com>
2025-07-14 10:09:21 +0800

    [update] 更新 CreateUserTransactionDto 中 IsSendMail 屬性的預設值為 true

--- src/EtheriT.Coker.Application.Shared/Dto/BonusManagement/CreateUserTransactionDto.cs
+++ src/EtheriT.Coker.Application.Shared/Dto/BonusManagement/CreateUserTransactionDto.cs
@@ -12,6 +12,10 @@ namespace EtheriT.Coker.Application.Shared.Dto.BonusManagement
         public string? TransactionOperation { get; set; }
         public int TransactionPoint { get; set; }
         public string? TransactionReason { get; set; }
-        public bool IsSendMail { get; set; } = false;
+
+        /// <summary>
+        /// 是否寄送紅利異動通知信件(預設啟用)
+        /// </summary>
+        public bool IsSendMail { get; set; } = true;
     }
 }

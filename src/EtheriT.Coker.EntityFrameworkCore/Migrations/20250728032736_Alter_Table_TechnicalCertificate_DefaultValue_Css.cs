f4430011e22203f54b81d4a847e8cbf9b5948e64
charles1984s <charles@ether.com.tw>
2025-07-28 14:02:16 +0800

    [update] 調整技術證照新增失敗問題

---
+++ src/EtheriT.Coker.EntityFrameworkCore/Migrations/20250728032736_Alter_Table_TechnicalCertificate_DefaultValue_Css.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EtheriT.Coker.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Alter_Table_TechnicalCertificate_DefaultValue_Css : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Html",
+                table: "TechnicalCertificates",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Css",
+                table: "TechnicalCertificates",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Html",
+                table: "TechnicalCertificates",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Css",
+                table: "TechnicalCertificates",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "");
+        }
+    }
+}

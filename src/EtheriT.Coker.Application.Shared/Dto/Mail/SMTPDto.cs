5391f9f5ab75fb95de2946a2842656c06f8208a5
charles1984s <charles@ether.com.tw>
2025-09-08 15:33:18 +0800

    [update] 調整mail server SSL加密選擇

--- src/EtheriT.Coker.Application.Shared/Dto/Mail/SMTPDto.cs
+++ src/EtheriT.Coker.Application.Shared/Dto/Mail/SMTPDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MailKit.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace EtheriT.Coker.Application.Shared.Dto.Mail
     {
         public string Url { get; set; } =  "msa.hinet.net";
         public int Port { get; set; } = 25;
-        public int UseSSL { get; set; } = 0;
+        public SecureSocketOptions UseSSL { get; set; } = SecureSocketOptions.Auto;
         public string? UserName { get; set; }
         public string? Password { get; set; }
     }

62ad5850e8986cf80ad1f2491b84feac46db54e7
LAPTOP-6GU1OHUO\user <m1020117@gmail.com>
2026-01-26 09:02:47 +0800

    [update]Order_Header加上物流所需欄位及Dto建立

---
+++ src/EtheriT.Coker.Application.Shared/Dto/ThirdParty/ECPayLogistics/ECPayLogisticsMapResponseDto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace EtheriT.Coker.Application.Shared.Dto.ThirdParty.ECPayLogistics
+{
+    public class ECPayLogisticsMapResponseDto
+    {
+        public string MerchantID { get; set; }
+        public string MerchantTradeNo { get; set; }
+        public string LogisticsSubType { get; set; }
+        public string CVSStoreID { get; set; }
+        public string CVSStoreName { get; set; }
+        public string CVSAddress { get; set; }
+        public string CVSTelephone { get; set; }
+        public string CVSOutSide { get; set; }
+        public string ExtraData { get; set; }
+    }
+}

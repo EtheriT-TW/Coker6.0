c291a8598e84fb71c32ad1fde099f39fd09c52e7
LAPTOP-6GU1OHUO\user <m1020117@gmail.com>
2026-02-03 00:21:31 +0800

    [update]綠界物流取得門市電子地圖微調(未完成)

--- src/EtheriT.Coker.Application.Shared/ThirdParty/IECPayLogisticsAppService.cs
+++ src/EtheriT.Coker.Application.Shared/ThirdParty/IECPayLogisticsAppService.cs
@@ -6,7 +6,7 @@ namespace EtheriT.Coker.Application.Shared.ThirdParty
 {
     public interface IECPayLogisticsAppService
     {
-        public Task<ResponseMessageDto> ECPayLogisticsGetMap(long scid, string LogisticsSubType);
+        public Task<ResponseMessageDto> ECPayLogisticsGetMap(string SCIds, string LogisticsSubType);
         public Task<bool> ECPayLogisticsGetMapResponse(ECPayLogisticsMapResponseDto ResultResponseData);
         public Task<ResponseMessageDto> ECPayLogisticsExpressCreate(long ohid, List<string> prod_titles);
         public Task<bool> ECPayLogisticsExpressCreateResponse(ECPayLogisticsCreateResponseDto ResultResponseData);

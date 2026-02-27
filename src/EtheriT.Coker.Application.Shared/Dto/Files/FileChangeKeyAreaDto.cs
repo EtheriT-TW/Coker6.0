cb6b5c79438b3820d7e60587b9a22391caf89b46
LAPTOP-6GU1OHUO\user <m1020117@gmail.com>
2026-02-27 16:30:17 +0800

    [update]載入無對應AreaKey檔案且可進行調整

---
+++ src/EtheriT.Coker.Application.Shared/Dto/Files/FileChangeKeyAreaDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EtheriT.Coker.Application.Shared.Dto.Files
+{
+    public class FileChangeKeyAreaDto
+    {
+        public long id { get; set; }
+        public long sid { get; set; }
+        public string areaKey { get; set; }
+    }
+}
+

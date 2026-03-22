2e2ed48957b5024c55722da8d105fa803c983ee0
LAPTOP-6GU1OHUO\user <m1020117@gmail.com>
2026-03-22 23:54:13 +0800

    [update]移除FileUpload的AreaKey

--- src/EtheriT.Coker.EntityFrameworkCore/Migrations/20260321084406_Update_Table_FileUpload_Remove_Areakey.Designer.cs
+++ src/EtheriT.Coker.EntityFrameworkCore/Migrations/20260321084406_Update_Table_FileUpload_Remove_Areakey.Designer.cs
@@ -4,6 +4,7 @@ using EtheriT.Coker.EntityFrameworkCore.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EtheriT.Coker.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(CokerDbContext))]
-    partial class CokerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260321084406_Update_Table_FileUpload_Remove_Areakey")]
+    partial class Update_Table_FileUpload_Remove_Areakey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

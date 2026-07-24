b49f4b59c633faf0a3efab9e80114505a96baa61
charles1984s <charles@ether.com.tw>
2026-07-24 17:03:52 +0800

    [update] Add GrapesJS editor adapter interface

--- packages/EtheriT.Coker.GrapesJS/src/index.js
+++ packages/EtheriT.Coker.GrapesJS/src/index.js
@@ -1,7 +1,13 @@
+import { createEditorAdapter } from './core/createEditorAdapter.js';
+
+export { createEditorAdapter };
+
 export function createCokerGrapesLibrary(options = {}) {
+    const adapter = options.adapter || createEditorAdapter(options);
+
     return {
         name: 'EtheriT.Coker.GrapesJS',
         version: '0.0.0',
-        options
+        adapter
     };
 }
